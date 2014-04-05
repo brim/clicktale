65fd23e18d0b2cf9f119be494a193c888b25894b
Brian McGilligan <brian@brimllc.com>
2014-04-04 22:44:22 -0400

    Removes the click tale blocks html from the cached page.

--- app/code/community/Brim/ClickTale/Model/Observer.php
+++ app/code/community/Brim/ClickTale/Model/Observer.php
@@ -5,12 +5,21 @@ class Brim_ClickTale_Model_Observer {
     public function saveResponseToCache(Varien_Event_Observer $observer) {
         if (($hash = Mage::registry('clicktale_hash'))) {
 
-            $response       = $observer->getFront()->getResponse();
+            try {
+                $response       = $observer->getFront()->getResponse();
 
-            $cacheProvider  = ClickTale_CacheFactory::DefaultCacheProvider();
-            $config         = ClickTale_Settings::Instance()->getCacheProviderConfig();
+                $cacheProvider  = ClickTale_CacheFactory::DefaultCacheProvider();
+                $config         = ClickTale_Settings::Instance()->getCacheProviderConfig();
 
-            $cacheProvider->store($hash, $response->getBody(), $config);
+                // remove clicktale blocks
+                $body = $response->getBody();
+
+                $newBody = preg_replace('/\<!\-\- ClickTale ([\w\d\s]+) \-\-\>(.*)\<!\-\- ClickTale end of \1 \-\-\>/siU', '', $body);
+
+                $cacheProvider->store($hash, $newBody, $config);
+            } catch (Exception $e) {
+                Mage::logException($e);
+            }
         }
     }
 }
